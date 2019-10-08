7f8c443f279de6f49dc6afc50a1ed934e7454787
Arthur Senko <arthursenko@gmail.com>
2019-10-08 16:01:39 -0700

    first commit

---
+++ src/components/Body.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Body extends Component {
+
+  render() {
+    return (
+      <div>
+        Body
+      </div>
+    );
+  }
+}
+
+export default Body;
