c29c4019b805c87729de3eeb32478c43975e650a
Miksiuk <smiksiuk@alluma.org>
2019-10-13 15:53:27 -0700

    login

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import store from '../redux/store';
+// import Loggedout from './Header/Loggedout';
+// import Loggedin from './Header/Loggedin';
 
 class Header extends Component {
 
-  changePage(page) {
+  changePage(page, mode) {
     store.dispatch({
       type: 'PAGE',
       payload: page,
+    });
+    if (mode !== undefined) store.dispatch({
+      type: 'MODE',
+      payload: mode,
     })
   }
 
@@ -19,12 +25,29 @@ class Header extends Component {
 
   render() {
     return (
-      <div></div>
+      <div>
+        {this.props.mode === 'loggedin' ?
+          <div className='card-header text-center'>
+            <button className="mr-1 btn-primary" onClick={() => this.changePage('loggedout', 'loggedout')}>Logout</button>
+            <button className="mr-1" onClick={() => this.changePage('users')}>Users</button>
+            <button className="mr-1" onClick={() => this.click()}>Increment</button>
+            <button onClick={() => this.changePage('about')}>About Us</button>
+          </div> :
+          <div className='card-header text-center'>
+            <button className="mr-1 btn-primary" onClick={() => this.changePage('loggedin', 'loggedin')}>Login</button>
+            <button className="mr-1" onClick={() => this.changePage('registration')}>Register</button>
+            <button className="mr-1" onClick={() => this.changePage('users')}>Users</button>
+            <button className="mr-1" onClick={() => this.click()}>Increment</button>
+            <button onClick={() => this.changePage('about')}>About Us</button>
+          </div>
+        }
+      </div >
     );
   }
 }
-const mapStateToProps = state => ({
 
+const mapStateToProps = state => ({
+  mode: state.mode
 })
 
 export default connect(mapStateToProps)(Header);
--- src/components/Header/Loggedout.js
+++ src/components/Header/Loggedout.js
@@ -5,12 +5,12 @@ class Loggedout extends Component {
   render() {
     return (
       <div className='card-header text-center'>
-        <button className="mr-1" onClick={() => this.changePage('login')}>Login</button>
+        <button className="mr-1 btn-primary" onClick={() => this.changePage('loggedin', 'loggedin')}>Login</button>
         <button className="mr-1" onClick={() => this.changePage('registration')}>Register</button>
         <button className="mr-1" onClick={() => this.changePage('users')}>Users</button>
         <button className="mr-1" onClick={() => this.click()}>Increment</button>
         <button onClick={() => this.changePage('about')}>About Us</button>
-      </div>
+      </div >
     );
   }
 }
